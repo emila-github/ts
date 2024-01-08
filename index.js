813b28819a813df645fe4f76931e2643c766d4b2
wangchangjian <wangchangjian_yyb@email.fjpicc.com>
2024-01-08 14:27:06 +0800

    函数的类型

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-// 1.字符串类型
-var a = '123';
-//普通声明
-//也可以使用es6的字符串模板
-var str = "dddd".concat(a);
-console.log(str);
-// 2.数字类型
-var num = 123; //普通数字
-var notANumber = NaN; //Nan
-var infinityNumber = Infinity; //无穷大
-var decimal = 6; //十进制
-var hex = 0xf00d; //十六进制
-var binary = 10; //二进制
-var octal = 484; //八进制s
-// 3.布尔类型
-// let createdBoolean: boolean = new Boolean(1)
-//这样会报错 应为事实上 new Boolean() 返回的是一个 Boolean 对象
-var n = null;
-var u = undefined;
-// tscconfig.json "strict": false 可以以下交差赋值
-// n = u
-// u = n
-// 4.空值类型
-function voidFn() {
-    console.log('test void');
-}
+// ts 可以定义this 的类型， 在js中无法使用 必须在第一个参数定义this 的类型
+var obj = {
+    user: [1, 2, 3],
+    add: function (num) {
+        this.user.push(num);
+    },
+    sub: function (num) {
+        var findIndex = this.user.findIndex(function (item) { return item === num; });
+        console.log('findIndex', findIndex);
+        if (findIndex != -1) {
+            this.user.splice(findIndex, 1);
+        }
+    },
+    otherFn: function (str) {
+        console.log(str);
+        return str;
+    },
+};
+obj.add(4);
+obj.otherFn('ttt');
+console.log(obj);
+obj.sub(2);
+console.log(obj);
--- index.ts
+++ index.ts
@@ -1,22 +1,31 @@
-function Arr1(...args: any): void {
-  console.log(arguments)
-  //错误的arguments 是类数组不能这样定义
-  let arr: number[] = arguments
-  console.log(arr)
+interface Obj {
+  user: number[]
+  add: (this: Obj, num: number) => void
+  sub: (this: Obj, num: number) => void
+  otherFn: (str: string) => string
 }
-Arr1(111, 222, 333)
 
-function Arr2(...args: any): void {
-  console.log(arguments)
-  //ts内置对象IArguments 定义
-  let arr: IArguments = arguments
-  console.log(arr)
+// ts 可以定义this 的类型， 在js中无法使用 必须在第一个参数定义this 的类型
+let obj: Obj = {
+  user: [1, 2, 3],
+  add(this: Obj, num: number) {
+    this.user.push(num)
+  },
+  sub: function (this: Obj, num: number) {
+    let findIndex = this.user.findIndex((item) => item === num)
+    console.log('findIndex', findIndex)
+    if (findIndex != -1) {
+      this.user.splice(findIndex, 1)
+    }
+  },
+  otherFn: (str) => {
+    console.log(str)
+    return str
+  },
 }
-Arr2(111, 222, 333)
 
-//其中 IArguments 是 TypeScript 中定义好了的类型，它实际上就是：
-interface IArguments {
-  [index: number]: any
-  length: number
-  callee: Function
-}
+obj.add(4)
+obj.otherFn('ttt')
+console.log(obj)
+obj.sub(2)
+console.log(obj)
