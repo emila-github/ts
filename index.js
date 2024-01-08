48922fd6d5792222c8e93cdb6f9d30c5c61c8183
wangchangjian <wangchangjian_yyb@email.fjpicc.com>
2024-01-08 15:05:24 +0800

    类型断言 | 联合类型 | 交叉类型

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-// ts 可以定义this 的类型， 在js中无法使用 必须在第一个参数定义this 的类型
-var obj = {
-    user: [1, 2, 3],
-    add: function (num) {
-        this.user.push(num);
-    },
-    sub: function (num) {
-        var findIndex = this.user.findIndex(function (item) { return item === num; });
-        console.log('findIndex', findIndex);
-        if (findIndex != -1) {
-            this.user.splice(findIndex, 1);
-        }
-    },
-    otherFn: function (str) {
-        console.log(str);
-        return str;
-    },
-};
-obj.add(4);
-obj.otherFn('ttt');
-console.log(obj);
-obj.sub(2);
-console.log(obj);
+function toBoolean(something) {
+    return something;
+}
+console.log(toBoolean(1)); // 返回值为 1
--- index.ts
+++ index.ts
@@ -1,31 +1,5 @@
-interface Obj {
-  user: number[]
-  add: (this: Obj, num: number) => void
-  sub: (this: Obj, num: number) => void
-  otherFn: (str: string) => string
+function toBoolean(something: any): boolean {
+  return something as boolean
 }
 
-// ts 可以定义this 的类型， 在js中无法使用 必须在第一个参数定义this 的类型
-let obj: Obj = {
-  user: [1, 2, 3],
-  add(this: Obj, num: number) {
-    this.user.push(num)
-  },
-  sub: function (this: Obj, num: number) {
-    let findIndex = this.user.findIndex((item) => item === num)
-    console.log('findIndex', findIndex)
-    if (findIndex != -1) {
-      this.user.splice(findIndex, 1)
-    }
-  },
-  otherFn: (str) => {
-    console.log(str)
-    return str
-  },
-}
-
-obj.add(4)
-obj.otherFn('ttt')
-console.log(obj)
-obj.sub(2)
-console.log(obj)
+console.log(toBoolean(1)) // 返回值为 1
