36f19831ccd9265f69de816853032a5df7895a17
wangchangjian <wangchangjian_yyb@email.fjpicc.com>
2024-01-10 10:55:51 +0800

    添加 Dom 节点 类 示例

--- index.js
+++ index.js
@@ -13,25 +13,75 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var A = /** @class */ (function () {
-    function A(name) {
-        this.name = name;
+var Dom = /** @class */ (function () {
+    function Dom() {
     }
-    A.prototype.print = function () {
-        return this.name;
+    Dom.prototype.createElement = function (el) {
+        return document.createElement(el);
     };
-    return A;
+    Dom.prototype.setText = function (el, text) {
+        el.textContent = text;
+    };
+    Dom.prototype.render = function (createElement) {
+        var _this = this;
+        var _a;
+        var el = this.createElement(createElement.tag);
+        if (createElement.children && Array.isArray(createElement.children)) {
+            createElement.children.forEach(function (item) {
+                var _a;
+                var child = _this.render(item);
+                _this.setText(child, (_a = item.text) !== null && _a !== void 0 ? _a : null);
+                el.appendChild(child);
+            });
+        }
+        else {
+            this.setText(el, (_a = createElement.text) !== null && _a !== void 0 ? _a : null);
+        }
+        return el;
+    };
+    return Dom;
 }());
-// new A() // 抽象类无法被实例化
-var B = /** @class */ (function (_super) {
-    __extends(B, _super);
-    function B() {
-        return _super.call(this, '迪迪') || this;
+var Vue = /** @class */ (function (_super) {
+    __extends(Vue, _super);
+    function Vue(options) {
+        var _this = _super.call(this) || this;
+        _this.options = options;
+        _this.init();
+        return _this;
     }
-    B.prototype.getName = function () {
-        return this.name;
+    Vue.version = function () {
+        return '1.0.0';
     };
-    return B;
-}(A));
-var b = new B();
-console.log(b.getName());
+    Vue.prototype.init = function () {
+        var app = typeof this.options.el == 'string'
+            ? document.querySelector(this.options.el)
+            : this.options.el;
+        var data = {
+            tag: 'div',
+            props: {
+                id: 1,
+                key: 1,
+            },
+            children: [
+                {
+                    tag: 'div',
+                    text: 'sub1',
+                },
+                {
+                    tag: 'div',
+                    text: 'sub2',
+                },
+            ],
+        };
+        app === null || app === void 0 ? void 0 : app.appendChild(this.render(data));
+        console.log(app);
+        this.mount(app);
+    };
+    Vue.prototype.mount = function (app) {
+        document.body.append(app);
+    };
+    return Vue;
+}(Dom));
+var v = new Vue({
+    el: '#app',
+});
--- index.ts
+++ index.ts
@@ -1,24 +1,98 @@
-abstract class A {
-  public name: string
-  constructor(name: string) {
-    this.name = name
-  }
-  print(): string {
-    return this.name
-  }
-  // 在A类定义了 getName 抽象方法但未实现
-  abstract getName(): string
+//1. class 的基本用法 继承 和 类型约束
+//2. class 的修饰符 readonly  private protected public
+//3. super 原理
+//4. 静态方法
+//5. get set
+interface Options {
+  el: string | HTMLElement
 }
 
-// new A() // 抽象类无法被实例化
+interface VueCls {
+  init(): void
+  options: Options
+}
 
-class B extends A {
-  constructor() {
-    super('迪迪')
+interface Vnode {
+  tag: string
+  text?: string
+  props?: {
+    id?: number | string
+    key?: number | string | object
   }
-  getName(): string {
-    return this.name
+  children?: Vnode[]
+}
+
+class Dom {
+  constructor() {}
+
+  private createElement(el: string): HTMLElement {
+    return document.createElement(el)
+  }
+
+  protected setText(el: Element, text: string | null) {
+    el.textContent = text
+  }
+
+  protected render(createElement: Vnode): HTMLElement {
+    const el = this.createElement(createElement.tag)
+    if (createElement.children && Array.isArray(createElement.children)) {
+      createElement.children.forEach((item) => {
+        const child = this.render(item)
+        this.setText(child, item.text ?? null)
+        el.appendChild(child)
+      })
+    } else {
+      this.setText(el, createElement.text ?? null)
+    }
+    return el
   }
 }
-let b = new B()
-console.log(b.getName())
+
+class Vue extends Dom implements VueCls {
+  options: Options
+  constructor(options: Options) {
+    super()
+    this.options = options
+    this.init()
+  }
+
+  static version() {
+    return '1.0.0'
+  }
+
+  public init() {
+    let app =
+      typeof this.options.el == 'string'
+        ? document.querySelector(this.options.el)
+        : this.options.el
+    let data: Vnode = {
+      tag: 'div',
+      props: {
+        id: 1,
+        key: 1,
+      },
+      children: [
+        {
+          tag: 'div',
+          text: 'sub1',
+        },
+        {
+          tag: 'div',
+          text: 'sub2',
+        },
+      ],
+    }
+    app?.appendChild(this.render(data))
+    console.log(app)
+
+    this.mount(app as Element)
+  }
+
+  public mount(app: Element) {
+    document.body.append(app)
+  }
+}
+
+const v = new Vue({
+  el: '#app',
+})
