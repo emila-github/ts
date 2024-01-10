157375b27a6e8b69ad5cecc001bffb5eea868200
wangchangjian <wangchangjian_yyb@email.fjpicc.com>
2024-01-10 10:17:14 +0800

    interface 定义 类

--- index.js
+++ index.js
@@ -13,56 +13,25 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-//定义类
-var Person = /** @class */ (function () {
-    function Person(name, age, some) {
-        // 在TypeScript是不允许直接在constructor 定义变量的 需要在constructor上面先声明
-        this.name = name;
-        this.age = age;
-        this.some = some;
-        // this.nb ='nb'  // 不可以通过this 去访问
-        // this.staticFn() // 静态函数 同样也是不能通过this 去调用
+var A = /** @class */ (function () {
+    function A() {
+        this.name = '1234';
     }
-    // static 静态函数 同样也是不能通过 this 去调用 也是通过类名去调用
-    Person.staticFn = function () {
-        console.log('staticFn');
-        return console.log(this.name);
-    };
-    Person.staticFn2 = function () {
-        console.log('staticFn2');
-        // 需注意： 如果两个函数都是static 静态的是可以通过this互相调用
-        return this.staticFn();
-    };
-    Person.prototype.run = function () { };
-    // 用static 定义的属性 不可以通过this 去访问 只能通过类名去调用
-    Person.nb = '123';
-    return Person;
+    return A;
 }());
-console.log('Person.nb', Person.nb);
-Person.staticFn();
-var xiaoming = new Person('小明', 28, 1);
-xiaoming.name;
-// xiaoming.age
-// xiaoming.some
-var Man = /** @class */ (function (_super) {
-    __extends(Man, _super);
-    function Man() {
-        var _this = _super.call(this, '小黑', 100, 2) || this;
-        console.log('Man constructor');
-        console.log(_this.name);
-        // console.log(this.age)
-        console.log(_this.some);
+// ts interface 定义类 使用关键字 implements   后面跟interface的名字多个用逗号隔开 继承还是用extends
+var Person = /** @class */ (function (_super) {
+    __extends(Person, _super);
+    function Person() {
+        var _this = _super.call(this) || this;
+        _this.asd = 'asd';
         return _this;
     }
-    Man.prototype.create = function () {
-        console.log('Man create');
-        console.log(this.name);
-        // console.log(this.age)
-        console.log(this.some);
+    Person.prototype.get = function (type) {
+        return type;
     };
-    return Man;
-}(Person));
-var man = new Man();
-man.name;
-// man.age
-// man.some
+    Person.prototype.set = function () { };
+    return Person;
+}(A));
+var man = new Person();
+console.log(man.name);
--- index.ts
+++ index.ts
@@ -1,61 +1,30 @@
-//定义类
-class Person {
-  // 使用public 修饰符 可以让你定义的变量 内部访问 也可以外部访问 如果不写默认就是public
-  public name: string
-  // 使用  private 修饰符 代表定义的变量私有的只能在内部访问 不能在外部访问
-  private age: number
-  // 使用  protected 修饰符 代表定义的变量私有的只能在内部和继承的子类中访问 不能在外部访问
-  protected some: any
-
-  // 用static 定义的属性 不可以通过this 去访问 只能通过类名去调用
-  static nb: string = '123'
-  // static 静态函数 同样也是不能通过 this 去调用 也是通过类名去调用
-  static staticFn() {
-    console.log('staticFn')
-    return console.log(this.name)
-  }
-  static staticFn2() {
-    console.log('staticFn2')
-    // 需注意： 如果两个函数都是static 静态的是可以通过this互相调用
-    return this.staticFn()
-  }
-
-  constructor(name: string, age: number, some: any) {
-    // 在TypeScript是不允许直接在constructor 定义变量的 需要在constructor上面先声明
-    this.name = name
-    this.age = age
-    this.some = some
-    // this.nb ='nb'  // 不可以通过this 去访问
-    // this.staticFn() // 静态函数 同样也是不能通过this 去调用
-  }
-  run() {}
+interface PersonClass {
+  get(type: boolean): boolean
 }
 
-console.log('Person.nb', Person.nb)
-Person.staticFn()
+interface PersonClass2 {
+  set(): void
+  asd: string
+}
 
-let xiaoming = new Person('小明', 28, 1)
-xiaoming.name
-// xiaoming.age
-// xiaoming.some
-
-class Man extends Person {
+class A {
+  name: string
   constructor() {
-    super('小黑', 100, 2)
-    console.log('Man constructor')
-    console.log(this.name)
-    // console.log(this.age)
-    console.log(this.some)
-  }
-  create() {
-    console.log('Man create')
-    console.log(this.name)
-    // console.log(this.age)
-    console.log(this.some)
+    this.name = '1234'
   }
 }
+// ts interface 定义类 使用关键字 implements   后面跟interface的名字多个用逗号隔开 继承还是用extends
+class Person extends A implements PersonClass, PersonClass2 {
+  asd: string
+  constructor() {
+    super()
+    this.asd = 'asd'
+  }
+  get(type: boolean): boolean {
+    return type
+  }
+  set(): void {}
+}
 
-let man = new Man()
-man.name
-// man.age
-// man.some
+let man = new Person()
+console.log(man.name)
