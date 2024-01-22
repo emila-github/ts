82f33ece664abe3436cdf35387a77e3cc307ce88
wangchangjian <wangchangjian_yyb@email.fjpicc.com>
2024-01-22 09:59:07 +0800

    接口枚举

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-var arr = [1, 'string'];
-arr.push(false); //error
-console.log(arr);
+var Types;
+(function (Types) {
+    Types[Types["yyds"] = 0] = "yyds";
+    Types[Types["dddd"] = 1] = "dddd";
+})(Types || (Types = {}));
+var obj = {
+    red: Types.yyds,
+};
