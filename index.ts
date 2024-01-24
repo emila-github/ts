fbec887e4339d56e9e34b33659d6c40f71bc2f39
wangchangjian <wangchangjian_yyb@email.fjpicc.com>
2024-01-24 10:40:36 +0800

    函数泛型

--- index.ts
+++ index.ts
@@ -1,28 +1,6 @@
-const obj = {
-  max: 5,
-  current: 0,
-  [Symbol.iterator]() {
-    return {
-      max: this.max,
-      current: this.current,
-      next() {
-        if (this.current == this.max) {
-          return {
-            value: undefined,
-            done: true,
-          }
-        } else {
-          return {
-            value: this.current++,
-            done: false,
-          }
-        }
-      },
-    }
-  },
+function Sub<T, K>(a: T, b: K): Array<T | K> {
+  const params: Array<T | K> = [a, b]
+  return params
 }
-console.log([...obj])
 
-for (let val of obj) {
-  console.log(val)
-}
+Sub<Boolean, number>(false, 1)
